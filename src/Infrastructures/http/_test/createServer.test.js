3fad115c502f0a54f8d7ad0984a832fa7a0fea96
rizkingrh <rizkingrh.mhd@gmail.com>
2026-06-27 19:27:12 +0700

    add say hello world test case

--- src/Infrastructures/http/_test/createServer.test.js
+++ src/Infrastructures/http/_test/createServer.test.js
@@ -32,6 +32,20 @@ describe('HTTP server', () => {
     expect(response.status).toEqual(404);
   });
 
+  describe('when GET /', () => {
+    it('should return 200 and hello world', async () => {
+      // Arrange
+      const app = await createServer({});
+
+      // Action
+      const response = await request(app).get('/');
+
+      // Assert
+      expect(response.status).toEqual(200);
+      expect(response.body.data).toEqual('Hello world!');
+    });
+  });
+
   describe('when POST /users', () => {
     it('should response 201 and persisted user', async () => {
       // Arrange
